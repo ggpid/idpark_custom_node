127794a105a65e69b8b2b4427d9644f7c80ad01d
ggpid <ggpid77@gmail.com>
2024-02-29 20:54:46 +0900

    fast

--- codewave_nodes.py
+++ codewave_nodes.py
@@ -1,10 +1,14 @@
+import os
+
+import folder_paths
 from server import PromptServer
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+from fastsam import FastSAM
 import numpy as np
 import torch
 import comfy
 from PIL import Image
-from tools import run_length_encode
+from tools import run_length_encode, resize_mask, resize_mask_centered
 
 
 class SAMGenerator:
@@ -30,26 +34,53 @@ class SAMGenerator:
     CATEGORY = "CODEWAVE"
 
     def doIt(self, sam_model, image):
-        if sam_model.is_auto_mode:
-            device = comfy.model_management.get_torch_device()
-            sam_model.safe_to.to_device(sam_model, device=device)
-
         numpy_image = np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
-
+        sam_model.to('cuda')
         mask_generator = SamAutomaticMaskGenerator(
             model=sam_model,
-            points_per_side=32,
-            pred_iou_thresh=0.86,
-            stability_score_thresh=0.92,
-            crop_n_layers=1,
-            crop_n_points_downscale_factor=2,
-            min_mask_region_area=100,  # Requires open-cv to run post-processing
         )
         masks_combined = mask_generator.generate(numpy_image)
 
         masks = [mask["segmentation"] for mask in masks_combined]
         masks_encoded = [run_length_encode(mask) for mask in masks]
+        return {"ui": {"masks": masks_encoded}}
 
-        message = {"node": "SAMGenerator", "output": {"masks": masks_encoded}}
-        PromptServer.instance.send_sync("executed", message)
+class FastSAMGenerator:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {
+            "required":
+                {"image": (sorted(files), {"image_upload": True})},
+        }
+
+    RETURN_TYPES = ()
+    # RETURN_NAMES = ("masks",)
+
+    FUNCTION = "doIt"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "CODEWAVE"
+
+    def doIt(self, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        input = Image.open(image_path)
+        input = input.convert("RGB")
+        fastsam_model = FastSAM('./FastSAM-x.pt')
+        everything_results = fastsam_model(
+            input,
+            device='cuda'
+        )
+        n_masks = everything_results[0].masks.data.cpu().numpy().squeeze()
+        masks_encoded = []
+        for mask in n_masks:
+            mask_resized = resize_mask_centered(mask, input.width, input.height)
+            mask_encoded = run_length_encode(mask_resized)
+            masks_encoded.append(mask_encoded)
+
         return {"ui": {"masks": masks_encoded}}
--- tools.py
+++ tools.py
@@ -1,5 +1,65 @@
 import numpy as np
+from scipy.ndimage import zoom
 
+def resize_mask_centered(mask, new_width, new_height):
+    """
+    이미지 마스크의 크기를 조정합니다. 중앙에서부터 이미지를 잘라내거나 0으로 채웁니다.
+    이를 통해 높이와 너비 모두 이미지의 중심에서 조정됩니다.
+
+    Parameters:
+    - mask (ndarray): 원본 이미지 마스크 (2차원 ndarray).
+    - new_width (int): 조정된 이미지의 새 너비.
+    - new_height (int): 조정된 이미지의 새 높이.
+
+    Returns:
+    - ndarray: 크기가 조정된 이미지 마스크.
+    """
+    current_height, current_width = mask.shape
+    resized_mask = np.zeros((new_height, new_width), dtype=mask.dtype)
+
+    # 시작점 계산 (원본 이미지 중앙에서 리사이징된 이미지의 중앙까지의 거리를 고려)
+    start_y = max((current_height - new_height) // 2, 0)
+    start_x = max((current_width - new_width) // 2, 0)
+
+    # 종료점 계산
+    end_y = start_y + min(current_height, new_height)
+    end_x = start_x + min(current_width, new_width)
+
+    # 리사이징된 이미지의 중앙에 원본 데이터를 삽입
+    resized_mask_start_y = max((new_height - current_height) // 2, 0)
+    resized_mask_start_x = max((new_width - current_width) // 2, 0)
+
+    resized_mask[resized_mask_start_y:resized_mask_start_y+min(current_height, new_height),
+    resized_mask_start_x:resized_mask_start_x+min(current_width, new_width)] = \
+        mask[start_y:end_y, start_x:end_x]
+
+    return resized_mask
+
+
+def resize_mask(mask, new_width, new_height):
+    """
+    2차원 ndarray 이미지 마스크의 너비와 높이를 원하는 값으로 조정합니다.
+    이미지의 원본 형태는 보존되어야 합니다.
+
+    Parameters:
+    - mask (ndarray): 원본 이미지 마스크 (2차원 ndarray).
+    - new_width (int): 조정된 이미지의 새 너비.
+    - new_height (int): 조정된 이미지의 새 높이.
+
+    Returns:
+    - ndarray: 조정된 이미지 마스크.
+    """
+    # 원본 이미지의 현재 너비와 높이를 계산
+    current_height, current_width = mask.shape
+
+    # 원하는 너비와 높이로 조정하기 위한 비율 계산
+    width_ratio = new_width / current_width
+    height_ratio = new_height / current_height
+
+    # 'nearest' 보간법을 사용하여 이미지 리사이징
+    resized_mask = zoom(mask, (height_ratio, width_ratio), order=0)
+
+    return resized_mask
 
 def run_length_encode(mask):
     """2차원 ndarray 마스크 데이터를 런랭스 압축하는 최적화된 함수"""
